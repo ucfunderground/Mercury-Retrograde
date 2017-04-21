44771c3d1316668c8875c649b6f92ea5b95cad34
ucfunderground <mercuryretrograde420@gmail.com>
2017-04-21 15:38:39 -0400

    Update common.h

--- C/common.h
+++ C/common.h
@@ -1,1 +1,33 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define string char*
+
+typedef enum facebookResponseTypes {
+  IGNORE,
+  POST,
+  COMMENT,
+  REPLY
+} FBResponseType;
+typedef enum astrologicSymbol {
+  CAPRICORN,
+  AQUARIUS,
+  PISCES,
+  ARIES,
+  TAURUS,
+  GEMINI,
+  CANCER,
+  LEO,
+  VIRGO,
+  LIBRA,
+  SCORPIO,
+  SAGGITARIUS
+} AstroSymbol;
+
+
+
+#endif
